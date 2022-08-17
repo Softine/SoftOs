b32a40044e68308a4488dc4c08140d89fcfc686e
Game Dev <77938761+Game-Dev2233@users.noreply.github.com>
2022-08-16 20:39:43 -0400

    Add files via upload

---
+++ compile.cmd
@@ -0,0 +1,11 @@
+@echo off
+title Compile Os
+cls
+i686-elf-as boot.s -o boot.o
+pause
+i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+pause
+i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+pause
+echo time to emulate
+qemu-system-i386 -kernel myos.bin
