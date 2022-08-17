b07fa5dd0b6f3e1e84e38cc5c33012e648134d3a
Game Dev <77938761+Game-Dev2233@users.noreply.github.com>
2022-08-16 20:55:57 -0400

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -4,11 +4,10 @@ cls
 del boot.o
 del kernel.o
 del myos.bin
+cls
 i686-elf-as boot.s -o boot.o
 pause
 i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
 pause
 i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
 pause
-echo time to emulate
-qemu-system-i386 -kernel myos.bin
